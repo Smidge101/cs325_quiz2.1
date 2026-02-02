066653c09830ffe7ac458ca283328f362f3558b9
Smidge101 <laurengoff99883@gmail.com>
2026-02-01 19:15:45 -0600

    update

--- calculator.py
+++ calculator.py
@@ -11,3 +11,19 @@ elif '-' in user_input:
     user_input2 = int(input("Enter second number: "))
     result = user_input1 - user_input2
     print(f"The result is: {result}")
+
+elif '*' in user_input:
+    user_input1 = int(input("Enter first number: "))
+    user_input2 = int(input("Enter second number: "))
+    result = user_input1 * user_input2
+    print(f"The result is: {result}")
+
+elif '/' in user_input:
+    user_input1 = int(input("Enter first number: "))
+    user_input2 = int(input("Enter second number: "))
+    if user_input2 != 0:
+        result = user_input1 / user_input2
+        print(f"The result is: {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+        
